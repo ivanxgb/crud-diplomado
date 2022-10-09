e75a59557bac5f861a6a4ec71f5fe85ac5e86cac
ivanxgb <ivanxgb@gmail.com>
2022-10-09 10:14:33 -0500

    Section for list fruits added and add fruit in html

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -9,31 +9,42 @@ const Formulario = () => {
       <h1 className="text-center">Formulario</h1>
       <hr />
 
-      <form>
-        <div className="form-group">
-          <label>Fruta</label>
-          <input
-            className="form-control mb-2"
-            type="text"
-            value={fruta}
-            onChange={({ target }) => setFruta(target.value)}
-            placeholder="Ingrese fruta"
-          />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Listado de Frutas</h4>
+          <ul className="list-group"></ul>
         </div>
-        <div className="form-group">
-          <label>Descripcion</label>
-          <input
-            className="form-control mb-2"
-            type="text"
-            value={descripcion}
-            onChange={({ target }) => setDescripcion(target.value)}
-            placeholder="Ingrese descripcion"
-          />
-          <button className="btn btn-primary w-100" type="submit">
-            Agregar
-          </button>
+
+        <div className="col-4">
+          <h4 className="text-center">Agregar Frutas</h4>
+
+          <form>
+            <div className="form-group">
+              <label>Fruta</label>
+              <input
+                className="form-control mb-2"
+                type="text"
+                value={fruta}
+                onChange={({ target }) => setFruta(target.value)}
+                placeholder="Ingrese fruta"
+              />
+            </div>
+            <div className="form-group">
+              <label>Descripcion</label>
+              <input
+                className="form-control mb-2"
+                type="text"
+                value={descripcion}
+                onChange={({ target }) => setDescripcion(target.value)}
+                placeholder="Ingrese descripcion"
+              />
+              <button className="btn btn-primary w-100" type="submit">
+                Agregar
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
